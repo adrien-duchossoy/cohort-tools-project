6f76fb723d857efaf5fac03a506a70bf5048bf4e
adrien-duchossoy <adrien.duchossoy@icloud.com>
2026-06-16 17:03:44 +0200

    feat : cohort db connection with mongoose

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose")
 const PORT = 5005;
+
+const Cohort = require("./models/Cohort.model")
+const Student = require("./models/Student.model")
 
 
 // setup CORS
@@ -9,8 +12,10 @@ const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -39,7 +44,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
@@ -50,7 +54,15 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.sendFile(__dirname + "/data/cohorts.json");
+  Cohort.find({})
+    .then((cohorts)=> {
+      console.log("Retrieved cohorts", cohorts)
+      res.json(cohorts)
+    })
+    .catch((error) => {
+      console.error("Error with cohorts:", error)
+      res.status(500).json({error: "Failed to retrieve cohorts"})
+    })
 });
 
 
